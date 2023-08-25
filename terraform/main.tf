3c3a0e426465056f59ed49267cc3ca4a7a549030
pklawit <pklawit@mysiaki.eu>
2023-08-25 11:16:26 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,4 +65,73 @@ resource "aws_route_table" "testrt" {
   }
 }
 
+# Security group for Wordpress Instance
+resource "aws_security_group" "wordpress-sg" {
+  name = "wordpress-sg"
+  description = "allow inbound traffic for ports 80,22"
+  vpc_id = aws_vpc.testvpc.id
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ping"
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wordpress-sg"
+  }
+}
+
+# Security group for MySQL instance
+resource "aws_security_group" "mysql-sg" {
+  name = "mysql-sg"
+  description = "allow traffic from wordpress instance to mysql"
+  vpc_id = aws_vpc.testvpc.id
+
+  ingress {
+    description = "mysql"
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [aws_security_group.wordpress-sg.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [ aws_security_group.wordpress-sg ]
+
+  tags = {
+    Name = "mysql-sg"
+  }
+}
 
